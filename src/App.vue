9b7bfc96e4722358d30fcaf91039fc8ee5da71f6
p0033 <p0033@student.jamk.fi>
2025-03-28 00:39:52 +0200

    Adding and deleting components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{title}}</h1>
+<MyModal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyModal from "./components/MyModal.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { MyModal },
+  data(){
+    return {
+      title: 'My 2nd Vue App'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    }
   }
 }
 </script>
@@ -22,5 +32,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.active {
+  background: rgb(53, 53, 124);
+  color: rgb(243, 242, 242);
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+button, input {
+  display: inline-block;
+  margin: 10px;
+
 }
 </style>
